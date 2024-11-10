9dcb5c5f73d3be932bdef3924a22627003101c2f
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-10 20:04:11 +0100

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,9 @@ import "./Contact.css";
 const Contact = ({ contact, deleteContact }) => (
   <li className="classContact">
     {contact.name} {contact.number}
-    <button onClick={() => deleteContact(contact.id)}>Delete</button>
+    <button onClick={() => deleteContact(contact.id)} className="btn">
+      Delete
+    </button>
   </li>
 );
 
